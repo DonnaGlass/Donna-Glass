1b8425df30a2cc8f41c9347abc789f13e42d17d9
DonnaGlass <aidoudchihab173@gmail.com>
2026-07-09 20:54:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-alert("Welcome to Donna Glass");
+function setFrench(){
+
+document.documentElement.lang="fr";
+document.body.dir="ltr";
+
+document.getElementById("menu1").innerHTML="Accueil";
+document.getElementById("menu2").innerHTML="À propos";
+document.getElementById("menu3").innerHTML="Produits";
+document.getElementById("menu4").innerHTML="Services";
+document.getElementById("menu5").innerHTML="Contact";
+
+document.getElementById("title").innerHTML="Bienvenue chez Donna Glass";
+
+document.getElementById("description").innerHTML=
+"Leader dans la fabrication et la transformation du verre pour le bâtiment, l'automobile, les appareils électroménagers et l'énergie solaire.";
+
+document.getElementById("slogan").innerHTML="LA PERFECTION";
+
+}
+
+function setArabic(){
+
+document.documentElement.lang="ar";
+document.body.dir="rtl";
+
+document.getElementById("menu1").innerHTML="الرئيسية";
+document.getElementById("menu2").innerHTML="من نحن";
+document.getElementById("menu3").innerHTML="المنتجات";
+document.getElementById("menu4").innerHTML="الخدمات";
+document.getElementById("menu5").innerHTML="اتصل بنا";
+
+document.getElementById("title").innerHTML="مرحباً بكم في Donna Glass";
+
+document.getElementById("description").innerHTML=
+"نقدم حلولاً احترافية في صناعة الزجاج للبناء والسيارات والأجهزة الكهرومنزلية والطاقة الشمسية.";
+
+document.getElementById("slogan").innerHTML="التميز في صناعة الزجاج";
+
+}
