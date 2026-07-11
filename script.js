83bdfdf12adced233b0462d60bb9a93916b6780b
Donna glass <aidoudchihab173@gmail.com>
2026-07-11 21:09:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -216,3 +216,48 @@ window.addEventListener("scroll", () => {
     lastScroll = currentScroll;
 
 });
+/* Navbar Scroll */
+
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 80){
+        navbar.classList.add("scrolled");
+    }else{
+        navbar.classList.remove("scrolled");
+    }
+
+});
+
+
+/* Active Menu */
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+
+        if(scrollY >= sectionTop){
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === "#" + current){
+            link.classList.add("active");
+        }
+
+    });
+
+});
